4d1645297c1a9cb4695a975e4883513fc82d030f
Forsteee <55308823+Forsteee@users.noreply.github.com>
2021-04-21 20:27:04 +0500

    test + brainUp

--- sqltoster/src/components/Library.tsx
+++ sqltoster/src/components/Library.tsx
@@ -6,35 +6,77 @@ import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import {ChangingButton} from "./layouts/Changingbutton";
 import AccordionList from "./layouts/Accordionlist";
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Box from '@material-ui/core/Box';
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import Accordion from "@material-ui/core/Accordion";
 
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root:{
-
+            flexGrow: 1,
+            backgroundColor:  theme.palette.background.paper,
+            display: 'flex',
+            height: 224,
         },
-        main:{
-
-        },
-        headT:{
-            textAlign:'center',
-            paddingTop:'1%',
-
-        },
-        btnG:{
-            textAlign:'center',
-            color:'white',
-            width:'100%',
-        },
-        heading: {
-            fontSize: theme.typography.pxToRem(15),
-            fontWeight: theme.typography.fontWeightRegular,
+        tabs: {
+            borderRight: `1px solid ${theme.palette.divider}`,
+            marginTop: '11%',
+            '&:hover': {
+                backgroundColor: '#6ADA6A',
+                color: 'white',
+            },
         },
         faq:{
-            padding: theme.spacing(1,0,1,0),
+
+        },
+        cont:{
+            backgroundColor: '#EDEEF0',
+            height: '100vh',
         },
     }),
 );
+
+
+
+/////vkladki
+interface TabPanelProps {
+    children?: React.ReactNode;
+    index: any;
+    value: any;
+}
+
+function TabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`vertical-tabpanel-${index}`}
+            aria-labelledby={`vertical-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box p={3}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+function a11yProps(index: any) {
+    return {
+        id: `vertical-tab-${index}`,
+        'aria-controls': `vertical-tabpanel-${index}`,
+    };
+}
+///
 
 export default function Library() {
     const classes = useStyles();
@@ -43,12 +85,11 @@ export default function Library() {
     const section1 = ({id:1,name:'Глава 1'});
     const section2 = ({id:2,name:'Глава 2'});
     const chapterList = [section1,section2];
-    const sectionItem1 = ({id:1,title:'1',content:'2'});
-    const sectionItem2 = ({id:2,title:'2',content:'3'});
-    const sectionItem3 = ({id:3,title:'3',content:'3sdfsdf'});
-    const sectionItem4 = ({id:4,title:'3',content:'4sdsf'});
-    const sectionList1 = [sectionItem1,sectionItem2];
-    const sectionList2 = [sectionItem3,sectionItem4];
+    const sectionItem1 = ({id:1,title:'1',content:'2',section_id:1});
+    const sectionItem2 = ({id:2,title:'2',content:'3',section_id:1});
+    const sectionItem3 = ({id:3,title:'3',content:'3sdfsdf',section_id:2});
+    const sectionItem4 = ({id:4,title:'3',content:'4sdsf',section_id:2});
+    const sectionList1 = [sectionItem1,sectionItem2,sectionItem3,sectionItem4];
     /*
     * const - массив с строками таблицы 'главы' ()
     * const - массив с строками таблицы 'содержание глав'
@@ -56,36 +97,92 @@ export default function Library() {
     *
     *
     * */
-    const [activeChapter,setActiveChapter]=React.useState(1);
-    const changeChapter=(id:number)=>{
-        setActiveChapter(id);
-        //запрос к бд по вытягиванию строки таблицы 'содержание глав' по id главы -> массив с содержанием главы (sectionList - сейчас), будем юзать setSectionList
-        //setSectionList(/*ответ от БД (response)*/)
-        //пока так (для теста)
-        if (id==1){
-            setSectionList(sectionList1);
-        }else{
-            setSectionList(sectionList2);
-        }
-        console.log(id);
+
+    /////vkladki
+    interface TabPanelProps {
+        children?: React.ReactNode;
+        index: any;
+        value: any;
     }
 
-    const [sectionList,setSectionList]=React.useState(sectionList1);
+    function TabPanel(props: TabPanelProps) {
+        const { children, value, index, ...other } = props;
+
+        return (
+            <div
+                role="tabpanel"
+                hidden={value !== index}
+                id={`vertical-tabpanel-${index}`}
+                aria-labelledby={`vertical-tab-${index}`}
+                {...other}
+            >
+                {value === index && (
+                    <Box p={3}>
+                        <Typography>{children}</Typography>
+                    </Box>
+                )}
+            </div>
+        );
+    }
+
+    function a11yProps(index: any) {
+        return {
+            id: `vertical-tab-${index}`,
+            'aria-controls': `vertical-tabpanel-${index}`,
+        };
+    }
+
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+        setValue(newValue);
+    };
+    /////
 
     return (
+
         <div className={classes.root}>
-            <Container maxWidth="lg" className={classes.main}>
-                <Typography component="div" variant="h5" className={classes.headT}>
-                    Материал
-                </Typography>
+            <Container maxWidth="md" className={classes.cont}>
                 <Grid container spacing={3} className={classes.faq}>
-                    <Grid item xs={8}>
-                        <AccordionList sections={sectionList}/>
+                    <Grid item xs={3}>
+            <Tabs
+                orientation="vertical"
+                variant="scrollable"
+                value={value}
+                onChange={handleChange}
+                aria-label="Vertical tabs example"
+                className={classes.tabs}
+            >
+                {chapterList.map((chapter) =>
+                    <Tab label={chapter.name} {...a11yProps(chapter.id)} />
+                )}
+            </Tabs>
                     </Grid>
-                    <Grid item xs={4}>
-                        <Paper elevation={3}>
-                            <ChangingButton onClickChapter={changeChapter} chapters={chapterList} />
-                        </Paper>
+                    <Grid item xs={9}>
+            {chapterList.map((chapter)=>
+                <TabPanel value={value} index={chapter.id-1}>
+                    {sectionList1.map((section)=>{
+                        if(section.section_id==chapter.id){
+                            return(
+                            <Accordion>
+                                <AccordionSummary
+                                    expandIcon={<ExpandMoreIcon />}
+                                    aria-controls="panel1a-content"
+                                    id="panel1a-header"
+                                >
+                                    <Typography >{section.title}</Typography>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    <Typography>
+                                        {section.content}
+                                    </Typography>
+                                </AccordionDetails>
+                            </Accordion>)
+                    }
+                        }
+                    )}
+                </TabPanel>
+            )}
                     </Grid>
                 </Grid>
             </Container>
--- sqltoster/src/components/Test.tsx
+++ sqltoster/src/components/Test.tsx
@@ -1,22 +1,165 @@
 import React from 'react';
-import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+import { Theme, createStyles, makeStyles,useTheme  } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
+        root:{
+        },
+        main:{
 
+        },
+        faq:{
+            padding: theme.spacing(1,0,1,0),
+        },
+        rightB:{
+            textAlign: 'right',
+        },
+        heightT:{
+            height:'228px',
+        },
     }),
 );
+
+interface TabPanelProps {
+    children?: React.ReactNode;
+    dir?: string;
+    index: any;
+    value: any;
+}
+
+function TabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`full-width-tabpanel-${index}`}
+            aria-labelledby={`full-width-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box p={3}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+function a11yProps(index: any) {
+    return {
+        id: `full-width-tab-${index}`,
+        'aria-controls': `full-width-tabpanel-${index}`,
+    };
+}
 
 export default function Test() {
 
     const classes = useStyles();
+    const theme = useTheme();
+
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+        setValue(newValue);
+    };
+
+    const handleChangeIndex = (index: number) => {
+        setValue(index);
+    };
+
+
+    const [answer, setAnswer] = React.useState('');
+
+    const handleChangeAnswer = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setAnswer(event.target.value);
+    };
+
+    const [standart, setStandart] = React.useState('Эталон из БД');
+
+    const handleChangeStandart = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setStandart(event.target.value);
+    };
 
     return (
-
-        <div>
-            <Container maxWidth="md">
-
+        <div >
+            <Container maxWidth="md" className={classes.main}>
+                <Grid container
+                      direction="column"
+                      justify="center"
+                >
+                    <Grid item>
+                <Grid container spacing={3} className={classes.faq}>
+                    <Grid item xs={4}>
+                        <Paper elevation={3}>
+                            задание
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={8}>
+                        <AppBar position="static" color="default">
+                            <Tabs
+                                value={value}
+                                onChange={handleChange}
+                                indicatorColor="primary"
+                                textColor="primary"
+                                variant="fullWidth"
+                                aria-label="full width tabs example"
+                            >
+                                <Tab label="Поле ввода" {...a11yProps(0)} />
+                                <Tab label="Эталон" {...a11yProps(1)} />
+                            </Tabs>
+                        </AppBar>
+                        <div className={classes.heightT}>
+                            <TabPanel value={value} index={0} dir={theme.direction}>
+                                <TextField
+                                    id=""
+                                    multiline
+                                    rows={5}
+                                    placeholder="Поле ввода"
+                                    variant="outlined"
+                                    value={answer}
+                                    onChange={handleChangeAnswer}
+                                    fullWidth
+                                />
+                                <div className={classes.rightB}>
+                                <Button variant="contained"
+                                        size="small"
+                                        color="primary">
+                                    Отправить
+                                </Button>
+                                </div>
+                            </TabPanel>
+                            <TabPanel value={value} index={1} dir={theme.direction}>
+                                <TextField
+                                    id=""
+                                    multiline
+                                    rows={5}
+                                    placeholder="Поле ввода"
+                                    variant="outlined"
+                                    value={standart}
+                                    fullWidth
+                                    disabled
+                                />
+                            </TabPanel>
+                        </div>
+                    </Grid>
+                </Grid>
+                    </Grid>
+                    <Grid item>
+                       <>{/*сделать независимые вкладки */}</>
+                    </Grid>
+                </Grid>
             </Container>
         </div>
     );
